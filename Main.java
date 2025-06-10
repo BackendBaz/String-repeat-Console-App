5ccc187e568bcca5834acca016f5ac47be21c3f5
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-06-10 05:09:40 -0700

    feat: implement user inputs (text and times) with the try-catch error management for InputMismatchException (times)

--- Main.java
+++ Main.java
@@ -1,6 +1,21 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hi there!");
+        try {
+            System.out.println("** String Repeat **\n");
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter your text: ");
+            String user_text = scanner.nextLine();
+            System.out.print("How many times do you want to repeat the text? ");
+            int user_times = scanner.nextInt();
+        }
+        catch(InputMismatchException e) {
+            System.out.println("\n'times' -> Please enter a number! Try again later.");
+        } finally {
+            System.out.println("\nBye!");
+        }
     }
 }
